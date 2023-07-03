feee52cdcf5406f89a873a236eb6cb35d19f04d1
davydavydavy05 <davydavydavy05@gmail.com>
2023-07-03 10:35:58 +0800

    remove front-end

--- front-end/src/config/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = 'http://localhost:8000/api'
-axios.defaults.headers.common['Accept'] = 'application/json'
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.withCredentials = true
-
-export default axios
